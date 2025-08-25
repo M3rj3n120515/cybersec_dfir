d6c3753ee23e6dabdb60d6a385f58b562a91211a
M3rj3n120515 <merwinpaul.arobo@pfda.gov.ph>
2025-08-25 15:40:58 +0800

    Add files via upload

---
+++ user script day 1.ps1
@@ -0,0 +1,10 @@
+﻿implementing data confidentialy
+using public keys:
+@coreBABA
+config t
+ip domain-name pfda.gov.ph
+crypto key generate rsa
+!!tanong anong keysize:256.512 1024
+ip ssh version 2
+ip ssh time-out 120
+end
